f8bdf2c56a02076ad381b9e28fbd5f7711730112
Nima Soufiani <n_soufiani@hotmail.com>
2024-08-18 22:50:56 +0100

    add migration files

--- sleep/admin.py
+++ sleep/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Sleep
+
+
 # Register your models here.
+@admin.register(Sleep)
+class SleepAdmin(admin.ModelAdmin):
+    list_display = ("id", "profile", "slept_at", "sleep_length")
+    readonly_fields = ("created_at", "updated_at")
--- sleep/models.py
+++ sleep/models.py
@@ -1,6 +1,15 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+
+from common.models import TimeStamp
+from user.models import Profile
 
 
 # Create your models here.
-class Sleep(models.Model):
-    slept
+class Sleep(TimeStamp):
+    profile = models.ForeignKey(Profile, on_delete=models.PROTECT)
+    slept_at = models.DateTimeField()
+    sleep_length = models.FloatField(validators=[MinValueValidator(0)], max_length=2)
+
+    def __str__(self):
+        return f"{self.profile.user.first_name} {self.profile.user.last_name} {self.slept_at}"
