e46bfbb3ad75cb28baa510e69aeb59c51ba6849a
Nima Soufiani <n_soufiani@hotmail.com>
2024-08-18 22:58:02 +0100

    display full name of foreign key relationship USER

--- user/admin.py
+++ user/admin.py
@@ -6,5 +6,8 @@ from .models import Profile
 # Register your models here.
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "age", "gender", "created_at", "updated_at")
+    list_display = ("id", "user_full_name", "age", "gender", "created_at", "updated_at")
     readonly_fields = ('created_at', 'updated_at')
+
+    def user_full_name(self, obj: Profile) -> str:
+        return f"{obj.user.first_name.title()} {obj.user.last_name.title()}"
