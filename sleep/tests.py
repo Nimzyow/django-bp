923aa6f47afc363884a88d455f825f201e49379d
Nima Soufiani <n_soufiani@hotmail.com>
2024-08-25 22:22:56 +0100

    complete sleep test

--- sleep/tests.py
+++ sleep/tests.py
@@ -1,3 +1,36 @@
+import json
+from datetime import timedelta
+
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.utils import timezone
+
+from sleep.models import Sleep
+from user.factories.factories import ProfileFactory
+from user.models import Profile
+
 
 # Create your tests here.
+class SleepCreate(TestCase):
+    def test_can_save_a_sleep_POST_request(self):
+        ProfileFactory.create_batch(1)
+        user = User.objects.first()
+
+        payload = {
+            "slept_at": (timezone.now() - timedelta(days=1)).isoformat(),
+            "sleep_length": 7.5,
+        }
+
+        self.client.login(username=user.username, password="password123")
+        response = self.client.post(
+            "/sleep/sleeps/", data=json.dumps(payload), content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 201)
+
+        sleep = Sleep.objects.get(profile=user.profile)
+
+        self.assertEqual(sleep.sleep_length, 7.5)
+
+        
+
