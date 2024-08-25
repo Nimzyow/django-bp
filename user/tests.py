c0e972a0028a9808284324294ff84d7bcb641a11
Nima Soufiani <n_soufiani@hotmail.com>
2024-08-25 21:43:03 +0100

    create get request

--- user/tests.py
+++ user/tests.py
@@ -2,12 +2,13 @@ import json
 
 from django.test import TestCase
 
+from user.factories.factories import ProfileFactory
 from user.models import User
 
 
 # Create your tests here.
-class UserListTest(TestCase):
-    def test_user_and_profile_created(self):
+class UserCreateListTest(TestCase):
+    def test_can_save_a_user_and_profile_POST_request(self):
         response = self.client.post(
             "/user/users/",
             data=json.dumps(
@@ -28,3 +29,9 @@ class UserListTest(TestCase):
         user = User.objects.first()
 
         self.assertEqual(user.first_name, "Karen")
+
+    def test_can_list_a_user_and_profile_GET_request(self):
+        ProfileFactory.create_batch(2)
+        response = self.client.get("/user/users/")
+
+        self.assertEqual(len(response.data), 2)
