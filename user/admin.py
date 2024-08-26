3f3a83643ebb038fd524768c048c67141d372295
Nima Soufiani <n_soufiani@hotmail.com>
2024-08-26 11:43:46 +0100

    amend admin

--- user/admin.py
+++ user/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
 
+from sleep.models import Sleep
+
 from .models import Profile
 
 
+class SleepInLine(admin.TabularInline):
+    model = Sleep
+    extra = 0
+    readonly_fields = ["slept_at", "sleep_length"]
+    ordering = ["-slept_at"]
+
+
 # Register your models here.
-@admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
+    inlines = [SleepInLine]
     list_display = ("id", "user_full_name", "age", "gender", "created_at", "updated_at")
     readonly_fields = ('created_at', 'updated_at')
 
     def user_full_name(self, obj: Profile) -> str:
         return f"{obj.user.first_name.title()} {obj.user.last_name.title()}"
+
+
+admin.site.register(Profile, ProfileAdmin)
